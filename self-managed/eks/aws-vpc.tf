4c081f36db0d4518e76fb704592991366370c52e
Eddie Rowe <74205376+eddie-rowe@users.noreply.github.com>
2024-01-19 15:11:15 -0600

    add rds isntance

--- self-managed/eks/aws-vpc.tf
+++ self-managed/eks/aws-vpc.tf
@@ -19,6 +19,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
+  create_database_subnet_group = true
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
